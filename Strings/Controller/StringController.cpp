2d5a1fd2be50da5c3727ded0fa2d0e384b0396bf
inperium <ibow9416@740s-pl-111.local>
2017-02-02 13:08:11 -0700

    Created base for Strings in C++ Project.

---
+++ Strings/Controller/StringController.cpp
@@ -0,0 +1,17 @@
+//
+//  StringController.cpp
+//  Strings
+//
+//  Created by Bowen, Isaac on 2/2/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#include <iostream>
+#include "StringController.hpp"
+
+using namespace std;
+
+void StringController :: start()
+{
+    
+}
---
+++ Strings/Controller/StringController.hpp
@@ -0,0 +1,20 @@
+//
+//  StringController.hpp
+//  Strings
+//
+//  Created by Bowen, Isaac on 2/2/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#ifndef StringController_h
+#define StringController_h
+
+class StringController
+{
+private:
+
+public:
+    void start();
+};
+
+#endif /* Controller_h */
